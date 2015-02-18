133412904cdd2ce04cddb3f65f716e448a1db78a
Matej Kollar <mkollar@redhat.com>
2015-02-18 18:23:10 +0100

    Small progress

---
+++ gen/test-html.hs
@@ -0,0 +1,86 @@
+{-# LANGUAGE OverloadedStrings #-}
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
+import qualified Text.Blaze.Html.Renderer.String as R
+-- import qualified Text.Blaze.Html.Renderer.Pretty as R
+
+import Control.Monad
+import Data.Time
+
+data Presentation = Presentation
+    { title :: String
+    , author :: String
+    , slides :: Maybe String
+    , audio :: Maybe String
+    , player :: Maybe String
+    } deriving Show
+
+data Meetup = Meetup
+    { indexM :: Integer
+    , presentations :: [Presentation]
+    , time :: UTCTime
+    , participants :: Integer
+    } deriving Show
+
+meetups =
+    [ Meetup
+        { indexM = 0
+        , presentations =
+            [ Presentation
+                { title = "There is no Compiler"
+                , author = "Matej"
+                , slides = Just "fpb-0/fpb-0.html"
+                , audio = Just "fpb-0/fpb-0.ogg"
+                , player = Just "fpb-0/player.html"
+                }
+            ]
+        , time = read "2014-02-16 19:00:00 CET"
+        , participants = 6
+        }
+    ]
+
+mFromMaybe_ :: Monad m => (a -> m ()) -> Maybe a -> m ()
+mFromMaybe_ = maybe (return ())
+
+presentation2html :: Presentation -> H.Html
+presentation2html p = H.div H.! A.class_ "presentation" $ do
+    H.toHtml (title p)
+    " by "
+    H.toHtml (author p)
+    h "Slides" slides
+    h "Audio" audio
+    h "Player" player
+    where
+    g t u = H.a H.! A.href (H.toValue u) $ t
+    h a b = mFromMaybe_ (g a) (b p)
+
+presentations2html :: [Presentation] -> H.Html
+presentations2html [] = "No presentations"
+presentations2html [x] = do
+    "Presentation: "
+    presentation2html x
+presentations2html s = do
+    "Presentatios: "
+    mapM_ presentation2html s
+
+meetup2html :: Meetup -> H.Html
+meetup2html x = do
+    H.h2 . H.toHtml $ "Meetup " ++ show (indexM x)
+    H.ul $ do
+        H.li $ presentations2html (presentations x)
+        H.li ("Time: " >> H.toHtml (show $ time x))
+        H.li ("Participants: " >> H.toHtml (show $ participants x))
+
+fpbTitle = "Functional Programming Brno"
+
+site = H.html $ do
+    H.head $ do
+        H.title fpbTitle
+        H.link H.! A.rel "stylesheet" H.! A.type_ "text/css" H.! A.href "style.css"
+    H.body $ H.div H.! A.class_ "wrapper" $ do
+        H.header $ do
+            H.h1 fpbTitle
+        H.p "More to come"
+        mapM_ meetup2html . take 10 $ reverse meetups
+
+main = writeFile "test.html" . R.renderHtml $ H.docType >> site
