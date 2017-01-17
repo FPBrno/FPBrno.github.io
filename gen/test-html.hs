e3b9a7554702c610cd7dd0fa3867d7f8b654dbe4
Peter Trško <peter.trsko@gmail.com>
2017-01-17 22:12:30 +0100

    Support for having sponsors for a meetup
    Kiwi.com offered to sponsor Meetup 7, now we have to find a way how to
    propagate this information.
    
    `Meetup` data type was extended to include list of sponsors. Concrete
    sponsors are represented as data constructors of `Sponsor` data type.
    It's the same idea as we have for `Tag`s, i.e. new sponsor, new data
    constructor.
    
    Rendering of `Sponsor` data type is more complex then rendering of `Tag`
    data type. Reason for this is that it represents a link, not just a
    text. For this reason I've introduced `HasLink` class that is very
    similar to `Show`, but the result represents URL for which `show`
    returns link value, i.e. sponsor name.

--- gen/test-html.hs
+++ gen/test-html.hs
@@ -8,9 +8,9 @@ import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 -- import qualified Text.Blaze.Html.Renderer.Pretty as R
 
-import Control.Monad (sequence, unless, zipWithM_)
+import Control.Monad (sequence, unless, when, zipWithM_)
 import Data.Default.Class (Default(def))
-import Data.List (intercalate, intersperse)
+import Data.List (intercalate, intersperse, uncons)
 import Data.Monoid
 import Data.Time
 import Data.Time.ISO8601 (formatISO8601)
@@ -29,6 +29,25 @@ data Tag
     | Types
     | Web
     deriving Show
+
+data Sponsor
+    = KiwiCom
+    | Ixperta
+
+instance Show Sponsor where
+    showsPrec _ = showString . \case
+        KiwiCom -> "Kiwi.com"
+        Ixperta -> "IXPERTA"
+
+class HasLink a where
+    getLink :: a -> URL
+
+instance HasLink Sponsor where
+    getLink = \case
+        KiwiCom -> "https://www.kiwi.com/"
+        Ixperta -> "http://www.ixperta.com/"
+
+-- TODO: Maybe we could add a logo for each sponsor.
 
 data Language
     = Cz
@@ -88,6 +107,7 @@ data Meetup = Meetup
     , participants :: Maybe Integer
     -- ^ Number of participats, for future, or meetups currently being held,
     -- this is set, obviously, to 'Nothing'.
+    , sponsors :: [Sponsor]
     } deriving Show
 
 possiblyMaybe = \case
@@ -100,6 +120,7 @@ futureMeetup idx = Meetup
     , presentations = []
     , time = Nothing
     , participants = Nothing
+    , sponsors = []
     }
 
 -- | Assign index to a 'Meetup'. It expects them to be in reverse order, i.e.
@@ -147,6 +168,7 @@ meetups = checkMeetupsIndex
             ]
         , time = Just $ read "2017-02-22 19:00:00 +02:00"
         , participants = Nothing
+        , sponsors = [KiwiCom]
         }
     , Meetup
         { indexM = 6
@@ -163,6 +185,7 @@ meetups = checkMeetupsIndex
             ]
         , time = Just $ read "2016-10-13 19:00:00 +02:00"
         , participants = Just 20
+        , sponsors = []
         }
     , Meetup
         { indexM = 5
@@ -179,6 +202,7 @@ meetups = checkMeetupsIndex
             ]
         , time = Just $ read "2016-07-27 18:00:00 +02:00"
         , participants = Just 12
+        , sponsors = []
         }
     , Meetup
         { indexM = 4
@@ -195,6 +219,7 @@ meetups = checkMeetupsIndex
             ]
         , time = Just $ read "2016-06-28 18:30:00 +02:00"
         , participants = Just 8
+        , sponsors = []
         }
     , Meetup
         { indexM = 3
@@ -211,6 +236,7 @@ meetups = checkMeetupsIndex
             ]
         , time = Just $ read "2015-11-25 18:30:00 +01:00"
         , participants = Just 28
+        , sponsors = []
         }
     , Meetup
         { indexM = 2
@@ -228,6 +254,7 @@ meetups = checkMeetupsIndex
             ]
         , time = Just $ read "2015-09-30 19:00:00 +02:00"
         , participants = Just 14
+        , sponsors = []
         }
     , Meetup
         { indexM = 1
@@ -244,6 +271,7 @@ meetups = checkMeetupsIndex
             ]
         , time = Just $ read "2015-05-12 18:00:00 +02:00"
         , participants = Just $ 4 + 18
+        , sponsors = []
         }
     , Meetup
         { indexM = 0
@@ -260,6 +288,7 @@ meetups = checkMeetupsIndex
             ]
         , time = Just $ read "2015-02-16 19:00:00 +01:00"
         , participants = Just 6
+        , sponsors = []
         }
     ]
 
@@ -322,7 +351,17 @@ meetup2html Meetup{..} = H.div H.! A.class_ "meetup" $ do
             "Time: "
             maybe "To be determined" time2Html time
         maybe mempty (\ n -> H.li ("Participants: " >> H.toHtml (show n))) participants
+        unless (null sponsors) . H.li $ do
+            "Sponsor" >> when (length sponsors > 1) "s" >> ": "
+            renderSponsors sponsors
         H.li $ presentations2html presentations
+  where
+    onJust x f = maybe mempty f x
+
+    renderSponsors = sequence_ . intersperse ", " . map renderSponsor
+
+    renderSponsor s =
+        H.a H.! A.href (H.toValue $ getLink s) $ H.toHtml (show s)
 
 fpbTitle :: H.Html
 fpbTitle = "Functional Programming Brno"
